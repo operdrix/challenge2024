a54776dca706218839f3ed632e0e92408f25b944
Mathis ROME <mathis.rome@icloud.com>
2024-03-04 17:58:51 +0100

    feat(admin-crud): Liste administateur

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
