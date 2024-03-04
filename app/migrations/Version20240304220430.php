1e07c239ed730ecabca234c9550056b42248a341
Olivier Perdrix <olivierperdrix@live.fr>
2024-03-04 23:05:35 +0100

    refactor(Entities): :construction: Add new migration file

---
+++ app/migrations/Version20240304220430.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240304220430 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE quiz_question ADD type VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE school CHANGE address address VARCHAR(255) DEFAULT NULL, CHANGE email email VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE school CHANGE address address VARCHAR(255) NOT NULL, CHANGE email email VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE quiz_question DROP type');
+    }
+}
