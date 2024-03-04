8a70a611a68738bc11c2011c2db1e50b31732551
Mathis ROME <mathis.rome@icloud.com>
2024-03-04 10:56:45 +0100

    feat(initialize-project): Initialisation projet

---
+++ app/src/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Page d'accueil
+ */
+class HomepageController extends AbstractController
+{
+    /**
+     * Page d'accueil
+     */
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
