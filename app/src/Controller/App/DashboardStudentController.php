a1c1ebdc7dac5ee8b54a2e7ea16d1aea7e0d892d
kenza schuler <kenza.schuler@gmail.com>
2024-03-05 15:18:15 +0100

    add trainings list for student

---
+++ app/src/Controller/App/DashboardStudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\App;
+
+use App\Entity\Training;
+use App\Repository\TrainingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DashboardStudentController extends AbstractController
+{
+    #[Route('/dashboard/student', name: 'app_dashboard_student')]
+    public function index(TrainingRepository $trainingRepository): Response
+    {
+        return $this->render('dashboard_student/index.html.twig', [
+            'trainings' => $trainingRepository->findAll(),
+        ]);
+    }
+}
