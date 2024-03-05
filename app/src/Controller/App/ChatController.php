c54a9f69d98fe52d52d87173cb016a5a57bbd9fa
Mathis ROME <mathis.rome@icloud.com>
2024-03-05 13:39:03 +0100

    feat(chat): Pagination et filtre

--- app/src/Controller/App/ChatController.php
+++ app/src/Controller/App/ChatController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\App;
 
+use App\Entity\Student;
+use App\Form\Type\StudentFilterType;
+use App\Service\FilteredListService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,16 +20,31 @@ class ChatController extends AbstractController
      * Liste des chats
      */
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(
+        FilteredListService $filteredListService,
+        Request $request
+    ): Response
     {
-        return $this->render('chat/index.html.twig');
+        [$pagination, $form] = $filteredListService->prepareFilteredList(
+            $request,
+            StudentFilterType::class,
+            Student::class,
+        );
+
+        return $this->render(
+            'chat/index.html.twig',
+            [
+                "pagination" => $pagination,
+                "form" => $form
+            ]
+        );
     }
 
     /**
      * Conversastio
      */
     #[Route("/{id}/conversation")]
-    public function conversation()
+    public function conversation(): Response
     {
         return $this->render("chat/conversation.html.twig");
     }
