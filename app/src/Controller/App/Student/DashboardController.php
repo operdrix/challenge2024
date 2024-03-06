0a55d745875b717d444753226772ed52306d376f
kenza schuler <kenza.schuler@gmail.com>
2024-03-06 19:44:27 +0100

    feat: add filter

--- app/src/Controller/App/Student/DashboardController.php
+++ app/src/Controller/App/Student/DashboardController.php
@@ -4,8 +4,10 @@ namespace App\Controller\App\Student;
 
 use App\Entity\Training;
 use App\Entity\Teacher;
+use App\Form\Type\TrainingFilterType;
 use App\Repository\TrainingRepository;
 use App\Repository\TrainingBlockRepository;
+use App\Service\FilteredListService;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,11 +20,21 @@ class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
     public function index(
+        FilteredListService $filteredListService,
+        Request $request,
         TrainingRepository $trainingRepository
     ): Response
     {
+        [$pagination, $form] = $filteredListService->prepareFilteredList(
+            $request,
+            TrainingFilterType::class,
+            Teacher::class
+        );
+
         return $this->render('student/dashboard/index.html.twig', [
-            'trainings' => $trainingRepository->findAll()
+            'trainings' => $trainingRepository->findAll(),
+            'pagination' => $pagination,
+            'form' => $form
         ]);
     }
 
@@ -30,6 +42,14 @@ class DashboardController extends AbstractController
     public function show(Training $training): Response
     {
         return $this->render('student/dashboard/show.html.twig', [
+            'training' => $training,
+        ]);
+    }
+
+    #[Route('/trainings/{id}/details', name: 'details', methods: ['GET'])]
+    public function details(Training $training): Response
+    {
+        return $this->render('student/dashboard/details.html.twig', [
             'training' => $training,
         ]);
     }
