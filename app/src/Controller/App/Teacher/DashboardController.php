2d1a9dea1f653c7a54e9fe7bda2ea53e65510112
Olivier PERDRIX <olivierperdrix@live.fr>
2024-03-06 14:57:01 +0100

    Divers Teacher

--- app/src/Controller/App/Teacher/DashboardController.php
+++ app/src/Controller/App/Teacher/DashboardController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/teacher', name: 'teacher_')]
 class DashboardController extends AbstractController
 {
-    #[Route('/teacher/dashboard', name: 'teacher_dashboard')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(
+        
+    ): Response
     {
         return $this->render('teacher/dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
