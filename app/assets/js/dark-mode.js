26c5cbe1cf66e5e5cbe89aa139d9218abc24b4a4
Olivier Perdrix <olivierperdrix@live.fr>
2024-03-06 23:58:26 +0100

    fix: dark mode bug

--- app/assets/js/dark-mode.js
+++ app/assets/js/dark-mode.js
@@ -4,10 +4,10 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
-    themeToggleDarkIcon.classList.add('hidden');
+    //themeToggleDarkIcon.classList.add('hidden');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
-    themeToggleLightIcon.classList.add('hidden');
+    //themeToggleLightIcon.classList.add('hidden');
 }
 
 var themeToggleBtn = document.getElementById('theme-toggle');
