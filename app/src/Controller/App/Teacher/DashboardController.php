a2dc16ad230aa6e9e5cd5f171899a623798fd50e
Olivier PERDRIX <olivierperdrix@live.fr>
2024-03-06 17:25:45 +0100

    feat: Ajout des statistiques étudiants

--- app/src/Controller/App/Teacher/DashboardController.php
+++ app/src/Controller/App/Teacher/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\App\Teacher;
 
+use App\Repository\GradeRepository;
+use App\Repository\StudentRepository;
+use App\Repository\TrainingRepository;
+use App\Repository\TrainingSessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,11 +15,23 @@ class DashboardController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(
-        
+        TrainingRepository $trainingRepository,
+        TrainingSessionRepository $trainingSessionRepository,
+        StudentRepository $studentRepository,
+        GradeRepository $gradeRepository
     ): Response
     {
+        $teacherTrainings = $trainingRepository->findBy(['teacher' => $this->getUser()]);
+        $nextTrainingsSessions = $trainingSessionRepository->findNextTrainingsSessions($this->getUser());
+        $studentsInGrade = $studentRepository->findStudentsWithGradeByTeacher($this->getUser());
+        $studentsOutGrade = $studentRepository->findStudentsWithoutGradeByTeacher($this->getUser());
+        $grades = $gradeRepository->findGradesByTeacher($this->getUser());
         return $this->render('teacher/dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'trainings' => $teacherTrainings,
+            'nextTrainingsSessions' => $nextTrainingsSessions,
+            'studentsInGrade' => $studentsInGrade,
+            'studentsOutGrade' => $studentsOutGrade,
+            'grades' => $grades
         ]);
     }
 }
