e3e42d2175ee1c86b3d3bd190f681e479e049670
kenza schuler <kenza.schuler@gmail.com>
2024-03-07 11:49:37 +0100

    Design page paramètres élève

--- app/src/Form/Type/StudentType.php
+++ app/src/Form/Type/StudentType.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\DateTimeImmutableType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,35 +23,59 @@ class StudentType extends AbstractType
             ->add('email', EmailType::class, [
                 'label' => 'Email',
                 'attr' => [
-                    'placeholder' => $isStudentParameterPage ? 'Email' : 'Email de l\'étudiant'
-                ]
+                    'placeholder' => $isStudentParameterPage ? 'Email' : 'Email de l\'étudiant',
+                    'class' => ''
+                ],
+                'row_attr' => [
+                    'class' => 'py-4'
+                ],
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
                 'attr' => [
                     'placeholder' => $isStudentParameterPage ? 'Prénom' : 'Prénom de l\'étudiant'
-                ]
+                ],
+                'row_attr' => [
+                    'class' => 'py-4'
+                ],
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom de famille',
                 'attr' => [
                     'placeholder' => $isStudentParameterPage ? 'Nom de famille' : 'Nom de famille de l\'étudiant'
-                ]
+                ],
+                'row_attr' => [
+                    'class' => 'py-4'
+                ],
             ]);
         if ($isStudentParameterPage) {
             $builder->add('address', TextType::class, [
                 'label' => 'Adresse',
                 'attr' => [
                     'placeholder' => 'Adresse'
-                ]
+                ],
+                'row_attr' => [
+                    'class' => 'py-4'
+                ],
             ])
             ->add('phoneNumber', TextType::class, [
                 'label' => 'Numéro de téléphone',
                 'attr' => [
                     'placeholder' => 'Numéro de téléphone'
-                ]
+                ],
+                'row_attr' => [
+                    'class' => 'py-4'
+                ],
             ])
-            ->add('birthdate');
+            ->add('birthdate', DateType::class, [
+                "widget" => "single_text",
+                'attr' => [
+                    'class' => 'py-4 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                ],
+                'row_attr' => [
+                    'class' => 'py-4'
+                ]
+            ]);
         }
     }
 
