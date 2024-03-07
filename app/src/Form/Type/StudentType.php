ef24ba1c5d00a2cc2fa8408fec43f70de2fc9cd1
Olivier PERDRIX <olivierperdrix@live.fr>
2024-03-07 10:09:13 +0100

    feat: Add CRUD Student Parameters

--- app/src/Form/Type/StudentType.php
+++ app/src/Form/Type/StudentType.php
@@ -4,6 +4,8 @@ namespace App\Form\Type;
 
 use App\Entity\Inscription;
 use App\Entity\Student;
+use Doctrine\DBAL\Types\DateImmutableType;
+use Doctrine\DBAL\Types\DateTimeImmutableType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -15,25 +17,41 @@ class StudentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $isStudentParameterPage = $options['action'] == 'isStudentParameterPage';
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Email',
                 'attr' => [
-                    'placeholder' => 'Email de l\'étudiant'
+                    'placeholder' => $isStudentParameterPage ? 'Email' : 'Email de l\'étudiant'
                 ]
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
                 'attr' => [
-                    'placeholder' => 'Prénom de l\'étudiabnt'
+                    'placeholder' => $isStudentParameterPage ? 'Prénom' : 'Prénom de l\'étudiant'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom de famille',
                 'attr' => [
-                    'placeholder' => 'Nom de famille de l\'étudiant'
+                    'placeholder' => $isStudentParameterPage ? 'Nom de famille' : 'Nom de famille de l\'étudiant'
                 ]
             ]);
+        if ($isStudentParameterPage) {
+            $builder->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Adresse'
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'Numéro de téléphone',
+                'attr' => [
+                    'placeholder' => 'Numéro de téléphone'
+                ]
+            ])
+            ->add('birthdate');
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
