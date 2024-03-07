43262c2f78aacb07ea8acd8102592f3b34f7113d
Mathis ROME <mathis.rome@icloud.com>
2024-03-07 15:18:36 +0100

    fix(training-repo): Missing filter

--- app/src/Repository/TrainingRepository.php
+++ app/src/Repository/TrainingRepository.php
@@ -36,7 +36,12 @@ class TrainingRepository extends ServiceEntityRepository
         }
 
         if (!empty($filters["student"])) {
-
+            $queryBuilder->leftJoin("t.inscriptions", "inscription")
+                ->leftJoin("inscription.students", "student1")
+                ->leftJoin("inscription.grade", "grade")
+                ->leftJoin("grade.students", "student2")
+                ->where("student1 = :student OR student2 = :student")
+                ->setParameter("student", $filters["student"]);
         }
 
         if (!empty($filters["title"])) {
