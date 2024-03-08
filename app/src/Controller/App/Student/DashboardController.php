8acd8cb26f1a9e8c8478cf8b46a065f48681ef56
BlueSkunka <quentin.somveille3874@gmail.com>
2024-03-08 01:31:54 +0100

    feat: dashboard student security

--- app/src/Controller/App/Student/DashboardController.php
+++ app/src/Controller/App/Student/DashboardController.php
@@ -21,8 +21,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/student', name: 'student_')]
+#[IsGranted('ROLE_STUDENT')]
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
@@ -57,6 +59,7 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/trainings/{id}', name: 'inscription_training', methods: ['GET'])]
+    #[IsGranted('view', 'inscription')]
     public function show(
         Inscription $inscription
     ): Response {
@@ -67,6 +70,7 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/trainings/{id}/progress', name: 'inscription_progress', methods: ['GET'])]
+    #[IsGranted('view', 'inscription')]
     public function inscriptionprogress(
         Inscription $inscription,
         ProgressServiceInterface $progressService
@@ -80,6 +84,7 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/trainings/{id}/details/{id_training_block}', name: 'inscription_training_block', methods: ['GET'])]
+    #[IsGranted('view', 'inscription')]
     public function details(
         Inscription $inscription,
         #[MapEntity(id: 'id_training_block')] TrainingBlock $trainingBlock,
@@ -109,6 +114,7 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/trainings/{id}/validate/{id_training_block}', name: 'inscription_training_block_validate', methods: ['GET'])]
+    #[IsGranted('view', 'inscription')]
     public function valdiate(
         Inscription $inscription,
         #[MapEntity(id: 'id_training_block')] TrainingBlock $trainingBlock,
