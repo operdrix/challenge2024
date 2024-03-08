7e702ad771708e4f58fcbb61d56692436684a0cf
BlueSkunka <quentin.somveille3874@gmail.com>
2024-03-08 01:31:54 +0100

    feat: dashboard student security

--- app/src/Security/Voter/TrainingInscriptionVoter.php
+++ app/src/Security/Voter/TrainingInscriptionVoter.php
@@ -5,7 +5,6 @@ namespace App\Security\Voter;
 use App\Entity\Teacher;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class TrainingInscriptionVoter extends Voter
 {
